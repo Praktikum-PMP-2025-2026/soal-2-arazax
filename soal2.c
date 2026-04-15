45d038da23657aa5f0291400dd8ccee56f98f0a2
Afdhal Razaq <mcrazaq01@gmail.com>
2026-04-15 08:35:57 +0700

    first commit

--- soal2.c
+++ soal2.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+int rekursif(int angka, int count) {
+    if (angka == 1){
+        return count;
+    }else if (angka % 2 == 0){
+        return rekursif(angka/2, count+1);
+    }else if (angka % 2 == 1){
+        return rekursif(3*angka + 1, count+1);
+    }
+}
+
+void hasil(int angka[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Langkah %d\n", rekursif(angka[i], 0));
+    }
+}
+
 int main(void) {
+    int n;
+    scanf("%d", &n);
+    int angka[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &angka[i]);
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d\n", angka[i]);
+    }
+    hasil(angka, n);
     return 0;
 }
